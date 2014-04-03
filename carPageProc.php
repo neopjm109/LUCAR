19b3977a9caaaee0d0713ddfbbc64fb11d8f8499
neopjm109 <neopjm109@gmail.com>
2014-04-03 22:58:18 +0900

    Modify carPageProc.php

--- carPageProc.php
+++ carPageProc.php
@@ -3,15 +3,15 @@
 	$sellNum = $_REQUEST['sell_num'];
 	
 	$carResult = mysqli_query($conn, "
-			select sp.img, sl.*
+			select sp.photo_url, sl.*
 			from sell_list as sl, sell_photo as sp
-			where sl.id = '$sellNum' and sl.id = sp.sell 
+			where sl.id = '$sellNum' and sl.id = sp.sell_list_id
 			");
 	
 	while ($row = mysqli_fetch_row($carResult)) {
 		echo "<div id=\"car_page_close\"><img src=\"img/close.png\" width=\"20\"></div>";
 		echo "<div id=\"car_page_img\">";
 		echo "<img src=\"$row[0]\"></div>";
-		echo "<div id=\"car_page_contents\">$row[1]<br>$row[2]<br>$row[4]<br>$row[5]<br>$row[6]<br>$row[7]</div>";
+		echo "<div id=\"car_page_contents\">$row[1]<br>$row[2]<br>$row[4]<br>$row[5]<br>$row[6]<br>$row[7]<br>$row[8]<br>$row[9]<br>$row[10]<br>$row[11]<br>$row[12]</div>";
 	}
 ?>
